0b16cf3093013d8d3f1f259aef29192a52fd5e9b
Luuqee <www.luucas@gmail.com>
2024-09-04 16:21:22 -0300

    subindo as clases e pacotes iniciais

---
+++ src/br/com/tech/ada/ProjetoB3/SimuladorInvestimentosB3.java
@@ -0,0 +1,9 @@
+package br.com.tech.ada.ProjetoB3;
+
+public class SimuladorInvestimentosB3 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/br/com/tech/ada/ProjetoB3/dominio/Carteira.java
@@ -0,0 +1,4 @@
+package br.com.tech.ada.ProjetoB3.dominio;
+
+public class Carteira {
+}
